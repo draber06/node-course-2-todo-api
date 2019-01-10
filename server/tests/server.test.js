fc170efa6f8f325e7dda64bf31d2d1ccc9452e41
Alexey Eremin <draber06@gmail.com>
2019-01-10 21:50:09 +0300

    Update npm modules and several tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -214,7 +214,7 @@ describe('PATCH /todos/:id', () => {
             .expect((res) => {
                 expect(res.body.todo.text).toBe(text);
                 expect(res.body.todo.completed).toBe(false);
-                expect(res.body.todo.completedAt).toBe(null);
+                expect(res.body.todo.completedAt).toBeFalsy();
             })
             .end(done);
     });
@@ -317,8 +317,7 @@ describe('POST /users/login', () => {
                 };
 
                 User.findById(users[1]._id).then((user) => {
-                    expect(user.tokens[1].access).toBe(response.access);
-                    expect(user.tokens[1].token).toBe(response.token);
+                    expect(user.toObject().tokens[1]).toMatchObject(response);
                     done();
                 }).catch((e) => done(e));
             });
